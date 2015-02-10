1d12e14a16ab7824c1f0ec328e503e3f60aee3ee
kznt <kznt@inbox.ru>
2015-02-10 18:13:11 +0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Art::Application.routes.draw do
 
   get 'hello/index'
 
+  get 'static_pages/about'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+
+=begin
+
+describe "StaticPages" do
+  describe "GET /static_pages" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get static_pages_index_path
+      response.status.should be(200)
+    end
+  end
+end
+
+=end
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Home')
+    end
+  end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Art | Home')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Art | Help')
+    end
+
+  end
+
+  describe "About page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Art | About')
+    end
+
+  end
